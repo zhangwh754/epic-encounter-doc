98ce377f02a2a74c0c80acbc400bcf75287c6d36
张文皓 <zhangwh754@163.com>
2024-03-24 13:57:47 +0800

    feat: img-zoom

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,9 +1,27 @@
 // .vitepress/theme/index.js
+import { onMounted, watch, nextTick } from "vue";
 import DefaultTheme from "vitepress/theme";
+import { useRoute } from "vitepress";
 import MyLayout from "./MyLayout.vue";
+import mediumZoom from "medium-zoom";
+
+import "./index.css";
 
 export default {
   extends: DefaultTheme,
   // 使用注入插槽的包装组件覆盖 Layout
   Layout: MyLayout,
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      mediumZoom(".main img", { background: "var(--vp-c-bg)" });
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+      () => route.path,
+      () => nextTick(() => initZoom())
+    );
+  },
 };
